62564b6625b139681081801b2d5359a1a53e597d
Nejc <nejc@poison.si>
2016-01-05 15:54:13 +0100

    Initial Commit

---
+++ ooze/ooze.h
@@ -0,0 +1,19 @@
+//
+//  ooze.h
+//  ooze
+//
+//  Created by Nejc on 5.1.2016.
+//  Copyright © 2016 nejc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ooze.
+FOUNDATION_EXPORT double oozeVersionNumber;
+
+//! Project version string for ooze.
+FOUNDATION_EXPORT const unsigned char oozeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ooze/PublicHeader.h>
+
+
